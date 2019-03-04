dccccf0d70ddbbba09fb3ec7f378273658409403
Juan Béjar <jabm97@gmail.com>
2019-03-04 17:43:21 +0100

    Primer commit
    Añadido formulario para registro de usuarios

---
+++ public_html/registro/registro-usuario.component.js
@@ -0,0 +1,22 @@
+angular.module("registroUsuario").component("registroUsuario", {
+  // This name is what AngularJS uses to match to the `<phone-list>` element.
+  templateUrl: "registro/registro-usuario.template.html",
+  controller: function RegistroUsuarioController($http, $scope) {
+    $scope.usuario = {};
+  
+    $scope.registrarUsuario = function(){
+        $http({
+            method: 'POST',
+            url: 'http://localhost:8080/usuarios',
+            data: $scope.usuario
+          })
+          .then(function (success) {
+            callback(success);
+          });
+
+    }
+    
+   
+
+}
+});
