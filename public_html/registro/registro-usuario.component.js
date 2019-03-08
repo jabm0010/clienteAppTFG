6e2946958d6ee0e12d2844268cf7373db712d275
Juan Béjar <jabm97@gmail.com>
2019-03-08 12:42:16 +0100

    Cambios en el registro

--- public_html/registro/registro-usuario.component.js
+++ public_html/registro/registro-usuario.component.js
@@ -1,22 +1,19 @@
 angular.module("registroUsuario").component("registroUsuario", {
   // This name is what AngularJS uses to match to the `<phone-list>` element.
-  templateUrl: "registro/registro-usuario.template.html",
+  templateUrl: "registro/registro-usuario2.template.html",
   controller: function RegistroUsuarioController($http, $scope) {
-    $scope.usuario = {};
+
+
+    $scope.usuario;
     $scope.contrasenaverificar = {};
-    $scope.registrarUsuario = function(){
-        $http({
-            method: 'POST',
-            url: 'http://localhost:8080/usuarios',
-            data: $scope.usuario
-          })
-          .then(function (success) {
-            callback(success);
-          });
-
-    }
-    
-   
-
-}
+    $scope.registrarUsuario = function() {
+      $http({
+        method: "POST",
+        url: "http://localhost:8080/usuarios",
+        data: $scope.usuario
+      }).then(function(success) {
+        callback(success);
+      });
+    };
+  }
 });
